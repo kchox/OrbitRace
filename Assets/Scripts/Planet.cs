e73006c39ce3a5d22aba6f2844dd22e1a6c5b192
sbelljr <samuel.bell.jr@gmail.com>
2014-09-22 22:52:28 -0400

    Initial prototype
    Made a simple planet prefab with simple gravity script; Made a player
    sphere with test input script: up/down to increase/decrease magnitude of
    current velocity; left/right to increase/decrease velocity perpendicular
    to origin.

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+	public GameObject player;
+	public float gravityStrength = 1;
+
+	private Vector3 gravityVec;
+	void FixedUpdate () {
+		gravityVec = this.transform.position - player.transform.position;
+		player.rigidbody.AddForce (gravityVec.normalized * gravityStrength/gravityVec.sqrMagnitude);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	void FixedUpdate ()
+	{
+		//float moveH = Input.GetAxis ("Horizontal");
+
+		// W/S keys or Up/Down arrows to increase/decrease magnitude of current velocity
+		float moveV = Input.GetAxis ("Vertical");
+			
+		Vector3 movement = rigidbody.velocity.normalized;
+			
+		rigidbody.AddForce (movement * speed * moveV * Time.deltaTime);
+
+
+
+
+		// A/D keys or Left/Right arrows to increase/decrease tangential velocity
+		//   relative to closest Planet
+		float moveH = Input.GetAxis ("Horizontal");
+		Vector3 movement2 = rigidbody.velocity.normalized - rigidbody.position * Vector3.Dot(rigidbody.velocity.normalized, rigidbody.position);
+		
+		rigidbody.AddForce (movement2.normalized * speed * moveH * Time.deltaTime);
+	}
+}
